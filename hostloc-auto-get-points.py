96de1ae3d31cf06a779ed1da86ac4ecaee5f7a11
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-02-27 22:31:36 +0800

    Update

--- hostloc-auto-get-points.py
+++ hostloc-auto-get-points.py
@@ -1,90 +1,80 @@
-import os
-import requests
-import time
-import random
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Author:  MoeClub.org
+
 import re
+import sys
+from urllib import request, parse
+from http import cookiejar
+import os
+
+username = os.getenv('HOSTLOC_USERNAME')
+passwd = os.getenv('HOSTLOC_PASSWD')
+account_dict = {
+    '0': {'username': username, 'password': passwd},
+}
 
 
-# 随机生成用户空间链接
-def randomly_gen_uspace_url():
-    url_list = []
-    # 访问小黑屋用户空间不会获得积分、生成的随机数可能会重复，这里多生成两个链接用作冗余
-    for i in range(12):
-        uid = random.randint(10000, 35000)
-        url = "https://www.hostloc.com/space-uid-{}.html".format(str(uid))
-        url_list.append(url)
-    return url_list
+def Login(URL, UserData):
+    _cookies = ''
+    _cookie = cookiejar.CookieJar()
+    _handler = request.HTTPCookieProcessor(_cookie)
+    _req = request.Request(URL, data=parse.urlencode(UserData).encode('utf-8'))
+    request.build_opener(_handler).open(_req)
+    for cookie in _cookie:
+        _cookies += cookie.name + '=' + cookie.value + ';'
+    return _cookies
 
 
-# 登录帐户
-def login(username, password):
-    headers = {
-        "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
-    }
-    login_url = "https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
-    login_data = {
-        "fastloginfield": "username",
-        "username": username,
-        "password": password,
-    }
-    s = requests.Session()
-    s.post(url=login_url, data=login_data, headers=headers)
-    return s
+def GetPage(URL, Header_Cookies):
+    _Header = {'Cookie': str(Header_Cookies)}
+    _req = request.Request(URL, headers=_Header)
+    return request.urlopen(_req).read().decode('utf-8')
 
 
-# 通过抓取用户设置页面的标题检查是否登录成功
-def check_login_status(s, number_c):
-    test_url = "https://www.hostloc.com/home.php?mod=spacecp"
-    res = s.get(test_url)
-    res.encoding = "utf-8"
-    test_title = re.findall("<title>.*?</title>", res.text)
-    if test_title[0] != "<title>个人资料 -  全球主机交流论坛 -  Powered by Discuz!</title>":
-        print("第" + str(number_c) + "个帐户登录失败！")
-        return False
+def GetCredit(user_data, proto='https'):
+    username = user_data['username']
+    Login_URL = proto + '://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
+    My_Credit = proto + '://www.hostloc.com/home.php?mod=spacecp&ac=credit&showcredit=1&inajax=1'
+    My_Home = proto + '://www.hostloc.com/home.php?mod=spacecp&inajax=1'
+    My_Cookies = Login(Login_URL, user_data)
+
+    if '<li>' not in GetPage(My_Home, My_Cookies):
+        print('[%s] Login Fail!' % username)
     else:
-        print("第" + str(number_c) + "个帐户登录成功！")
-        return True
+        try:
+            CreditNum0 = str(re.findall('[0-9]+', GetPage(My_Credit, My_Cookies))[-1])
+        except:
+            CreditNum0 = 'Null'
+        for x in range(25297, 25309):
+            GetPage(proto + '://www.hostloc.com/space-uid-{}.html'.format(x), My_Cookies)
+        try:
+            if CreditNum0 == 'Null':
+                raise Exception
+            CreditNum1 = str(re.findall('[0-9]+', GetPage(My_Credit, My_Cookies))[-1])
+            if CreditNum0 == CreditNum1:
+                CreditDetails = str(CreditNum1)
+            else:
+                CreditDetails = str(CreditNum0) + '->' + str(CreditNum1)
+        except:
+            CreditDetails = 'Null'
+        print('[%s] Login Success! (Credit: %s)' % (username, CreditDetails))
 
 
-# 依次访问随机生成的用户空间链接获取积分
-def get_points(s, number_c):
-    if check_login_status(s, number_c):
-        url_list = randomly_gen_uspace_url()
-        # 使用for和try/except实现当前链接访问出错时不中断程序继续访问下一个链接
-        for url in url_list:
-            try:
-                s.get(url)
-                print("用户空间链接：" + url + " 访问成功")
-                time.sleep(4)  # 每访问一个链接后休眠4秒，以避免触发论坛的防cc机制
-            except Exception as e:
-                print("链接访问异常：" + str(e))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        n = 0
+        account_dict = {}
+        account_list = [sys.argv[x] for x in range(1, len(sys.argv))]
+        for account in account_list:
+            if ":" not in account:
+                continue
+            account_dict[str(n)] = {}
+            account_dict[str(n)]['username'] = str(str(account).split(":", 1)[0])
+            account_dict[str(n)]['password'] = str(str(account).split(":", 1)[-1])
+            n += 1
+    for i in range(0, len(account_dict)):
+        try:
+            GetCredit(account_dict[str(i)])
+        except:
             continue
-    else:
-        print("请检查你的帐户是否正确！")
-
-
-if __name__ == "__main__":
-    username = os.environ["HOSTLOC_USERNAME"]
-    password = os.environ["HOSTLOC_PASSWORD"]
-
-    # 分割用户名和密码为列表
-    user_list = username.split(",")
-    passwd_list = password.split(",")
-
-    if len(user_list) != len(passwd_list):
-        print("用户名与密码个数不匹配，请检查环境变量设置是否错漏！")
-    else:
-        print("共检测到" + str(len(user_list)) + "个帐户，开始获取积分")
-        print("**************************************************")
-
-        # 使用for和try/except实现当前用户获取积分出错时不中断程序继续尝试下一个用户
-        for i in range(len(user_list)):
-            try:
-                s = login(user_list[i], passwd_list[i])
-                get_points(s, i + 1)
-                print("**************************************************")
-            except Exception as e:
-                print("获取积分异常：" + str(e))
-            continue
-
-        print("程序执行完毕，获取积分结束")
